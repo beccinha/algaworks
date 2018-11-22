23dbd9a63c8613e3b62a62114fd1155f3fa30835
beccinha <rhsnn@ecomp.poli.br>
2018-11-22 15:47:34 -0300

    filtros lancamento

---
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,67 @@
+package com.example.algamoney.api.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.service.LancamentoService;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+	
+	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private LancamentoService lancamentoService;
+	
+	@GetMapping
+	public List<Lancamento> pesquisar(LancamentoFilter filter ){
+		return lancamentoRepository.findAll();
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+		
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo){
+		Lancamento lancamento = lancamentoRepository.findOne(codigo);
+		return lancamento !=null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
+	}
+	
+
+}
---
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -0,0 +1,60 @@
+package com.example.algamoney.api.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.algamoney.api.exceptionhendler.AlgamoneyExceptionHendler.Erro;
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.PessoaRepository;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@Service
+public class LancamentoService {
+	
+	@Autowired
+	 private LancamentoRepository lancamentoRepository;
+		
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoPeloCodigo(codigo);
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+	
+	private Lancamento buscarLancamentoPeloCodigo(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		return lancamentoSalvo;	
+	}
+	
+	public Lancamento salvar(Lancamento lancamento) {
+		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		if(pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		return lancamentoRepository.save(lancamento);
+	}
+	
+	@ExceptionHandler({PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlerPessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex){
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = messageSource.toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		
+		return ResponseEntity.badRequest().body(erros);
+	}
+}
