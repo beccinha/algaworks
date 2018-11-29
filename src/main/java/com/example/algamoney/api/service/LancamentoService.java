f8c95064ed1bb747f57b153e79e4b64c7c0c5d4a
beccinha <rhsnn@ecomp.poli.br>
2018-11-29 12:47:11 -0300

    atualizacao lancamento

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -31,11 +31,34 @@ public class LancamentoService {
 	private PessoaRepository pessoaRepository;
 	
 	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
-		Lancamento lancamentoSalvo = buscarLancamentoPeloCodigo(codigo);
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		
+		if(!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
 		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
 		return lancamentoRepository.save(lancamentoSalvo);
 	}
 	
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		if(lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if(lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		}
+		
+		if(pessoa == null || pessoa.isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+	}
+
 	private Lancamento buscarLancamentoPeloCodigo(Long codigo) {
 		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
 		return lancamentoSalvo;	
